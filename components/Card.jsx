f56c51c0a64323314aab9170a9d94107615f89e8
Kerrion Siokwu <ladykerrion@sky.com>
2023-10-23 15:00:36 +0100

    Integration: Add styled Card Component

---
+++ components/Card.jsx
@@ -0,0 +1,30 @@
+import Marker from "../src/assets/images/marker.svg";
+
+export default function Card(props) {
+  return (
+    <>
+      <div className="cardItem d-flex flex-row justify-content-center">
+        <img
+          src={props.cardItem.imageUrl}
+          alt={props.cardItem.alt}
+          className="cardItem--photo"
+        />
+        <div className="cardItem--info-container">
+          <div className="location-grp d-flex align-items-center">
+            <img src={Marker} alt="location marker" className="location-marker" />
+            <p className="cardItem--info-location">{props.cardItem.location}</p>
+            <p className="cardItem--info-mapLink">
+              <a href={props.cardItem.googleMapsUrl}>View on Google Maps</a>
+            </p>
+          </div>
+          <h2 className="cardItem--title">{props.cardItem.title}</h2>
+          <p className="cardItem--date">
+            {props.cardItem.startDate} - {props.cardItem.endDate}
+          </p>
+          <p className="cardItem--description">{props.cardItem.description}</p>
+        </div>
+      </div> 
+      <hr />
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import data from "../data/data";
 import "../src/assets/css/App.css";
 
 export default function App() {
+  const cards = data.map((cardItem) => {
+    return <Card 
+      key={cardItem.id}
+      cardItem={cardItem}
+    />
+  })
   return (
     <>
       <Navbar />
+      <section className="cards-list d-flex flex-column">
+        {cards}
+      </section>
     </>
   );
 }
